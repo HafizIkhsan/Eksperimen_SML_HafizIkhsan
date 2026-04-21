2bea67ace75729a9ded78219519893b7f75c10cf
Hafiz Ikhsan <hafizikhsan73@gmail.com>
2026-04-21 17:57:55 +0700

    Menambahkan kode untuk menyimpan hasil pada ipynb dan menyesuaikan kembali automate.py

--- preprocessing/automate_HafizIkhsan.py
+++ preprocessing/automate_HafizIkhsan.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-
 BASE_DIR = Path(__file__).resolve().parent
 INPUT_PATH = BASE_DIR.parent / "loan_approval_raw.csv"
 OUTPUT_PATH = BASE_DIR / "loan_approval_preprocessed.csv"
@@ -22,15 +21,14 @@ def preprocessing_data(df: pd.DataFrame):
     for column in categorical_columns:
         df[column] = label_encoder.fit_transform(df[column])
     
+    # Menyimpan hasil preprocessing ke file baru
+    df.to_csv(OUTPUT_PATH, index=False)
     return df
 
 
 def main():
     df = pd.read_csv(INPUT_PATH)
-    df_processed = preprocessing_data(df)
-    df_processed.to_csv(OUTPUT_PATH, index=False)
-    print(f"Preprocessing selesai. File tersimpan di: {OUTPUT_PATH}")
-
+    preprocessing_data(df)
 
 if __name__ == "__main__":
     main()
